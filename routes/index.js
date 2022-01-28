a33be3a28b41c0cc319d1047bc6c6ead021b6c39
Christine Frances Betten <christinebetten@gmail.com>
2022-01-28 15:40:52 +0100

    Add postgres server

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+const { Client } = require('pg');
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV == 'production' ? { rejectUnauthorized: false } : false
+});
+
+client.connect();
+
+router.get('/dbtime', function(req, res, next) {
+    client.query('SELECT NOW()', (err, dbResult) => {
+      if (err) {
+        next(err)
+      } else {
+        res.send({now: dbResult.rows[0]['now']})
+      }
+    });
+});
+
 module.exports = router;
